7d526a13413665cbc7968321128a435a6c71cf40
situtsek <kestutis.milut@gmail.com>
2017-11-17 21:20:37 +0200

    uzduotys

---
+++ 1-1 uzduotys/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_uzduotys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "Kestutis";
+            var pavarde = "Milutis";
+            var amzius = 39;
+            var ugis = 1.78f;
+            var svoris = 80; // kg
+            var aukstoji_mokykla = "VUKHF";
+            var akademines_grupes_kodas = 'A';
+            var kursas = 2;
+            var studiju_programos_pavadinimas = "vadyba";
+            var atsiskaitymu_kreditu_skaicius = 5;
+
+
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveikas, Pasaulį!");
+            Console.ReadKey();
+            Console.WriteLine();
+          
+        }
+    }
+}
